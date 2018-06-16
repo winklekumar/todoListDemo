c479d10e70b158a4e38928b28ea8f9e3d234574d
Pravesh Kumar <praveshkumar@192-168-1-6.tpgi.com.au>
2018-06-16 12:51:50 +1000

    boot strap dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// Add bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
