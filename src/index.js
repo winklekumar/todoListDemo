50a8b637f01f0840471bd4f83eefcc42d3350b55
Pravesh Kumar <praveshkumar@192-168-1-6.tpgi.com.au>
2018-06-16 13:20:16 +1000

    static list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-
-// Add bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
+import './style/index.css';
 
 ReactDOM.render(
   <App />,
